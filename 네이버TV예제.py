8b9fce1776d5b5113efdd8f563afe5a9f16cb619
unknown <swh9870@naver.com>
2021-11-09 15:26:49 +0900

    merge

---
+++ 네이버TV예제.py
@@ -0,0 +1,25 @@
+
+# 1. 패키지 install
+
+import requests
+from bs4 import BeautifulSoup
+import openpyxl
+
+wb = openpyxl.Workbook()
+
+sheet= wb.active
+
+sheet.append(['제목','채널명','조회수','좋아야수'])
+
+raw = requests.get('https://tv.naver.com/r')
+html = BeautifulSoup(raw.text,'html.parser')
+
+container = html.select('div.inner')
+
+for con in container:
+    t = con.select_one("dt.title").text.strip() #제목
+    c = con.select_one("dd.chn").text.strip() #채널
+    h = con.select_one("span.hit").text.strip() #조회수
+    l = con.select_one("span.like").text.strip() #좋아요수
+    sheet.append([t,c,h,l])
+wb.save('naver_tv.xlsx')
---
+++ 뉴스기사.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+import openpyxl
+
+wb = openpyxl.Workbook()
+sheet = wb.active
+
+search  = '유미의세포'
+
+sheet.append([search,'기사제목','기사요약'])
+
+
+for p in range(1,10+1):
+    raw = requests.get("https://search.daum.net/search?w=news&DA=PGD&enc=utf8&cluster=y&cluster_page=1&q={s}&p={p}".format(s = search, p = p))
+    html =BeautifulSoup(raw.text,'html.parser')
+    container = html.select('ul.list_news li')
+    for c in container:
+        # 기사 제목: div.wrap.tit a
+        title = c.select_one("a.tit_main.fn_tit_u").text.strip()
+
+        content = c.select_one("p.desc").text.strip()
+        sheet.append([search,title,content])
+
+wb.save('daum_news.xlsx')
+
